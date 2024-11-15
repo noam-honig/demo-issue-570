d05e1b9f67a25b531a9cb3a107672c90925f2025
Noam Honig <noam.honig@gmail.com>
2024-11-15 10:47:31 +0200

    Demo

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,40 @@
 import express from "express";
-import { api } from "./api.js";
+import { Entity, Fields, JsonDataProvider, remult, Remult } from "remult";
+import { remultExpress } from "remult/remult-express";
+import { JsonEntityFileStorage } from "remult/server";
 
 const app = express();
 
+@Entity("Task", {
+  allowApiCrud: true,
+  saved: async (task, e) => {
+    await remult.repo(Task).deleteMany({ where: { id: undefined } });
+  },
+})
+class Task {
+  @Fields.uuid()
+  id!: string;
+
+  @Fields.string()
+  title!: string;
+}
+
+export const api = remultExpress({
+  entities: [Task],
+  dataProvider: async () =>
+    new JsonDataProvider(new JsonEntityFileStorage("./db")),
+  initApi: async (remult: Remult) => {
+    await remult.repo(Task).insert({ title: "New Task" });
+
+    const deleted = await remult.repo(Task).deleteMany({
+      where: { title: undefined },
+    });
+    console.log("Deleted tasks: ", deleted); // result: "Deleted tasks: 1"
+  },
+});
+
 app.use(api);
 
-// This code is responsible for serving the frontend files.
-const frontendFiles = process.cwd() + "/dist";
-app.use(express.static(frontendFiles));
-app.get("/*", (_, res) => {
-  res.sendFile(frontendFiles + "/index.html");
+app.listen(3001, () => {
+  console.log("Server is running on http://localhost:3001");
 });
-// end of frontend serving code
-
-app.listen(process.env["PORT"] || 3002, () => console.log("Server started"));
